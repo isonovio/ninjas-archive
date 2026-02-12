b3f1dce0386507871ae7f0958ca2f2e61096f5a2
Yinong Wang <49606461+IsoNovio@users.noreply.github.com>
2026-02-12 01:52:56 -0600

    code: best practices

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { GenreFilter } from "$lib/types/timeline-filter";
     import Timeline from "$lib/components/timeline/Timeline.svelte";
+    import { Genre } from "$lib/types/timeline-genre";
 
-    const prefilter = new GenreFilter("newspiece");
+    const prefilter = new GenreFilter(Genre.NEWSPIECE);
 </script>
 
 <svelte:head>
